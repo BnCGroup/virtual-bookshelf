afe0e23ecba756aa020d5afd2ac4e4df0f7be60e
C45tr0 <46m2.b34v3r@gmail.com>
2018-11-08 00:42:12 -0700

    Adding Loading screen that will preload assets and start loading processes

---
+++ src/lib/mobile/screens/loading/index.tsx
@@ -0,0 +1,67 @@
+import Axios from 'axios';
+import { AppLoading, Permissions } from 'expo';
+import * as React from 'react';
+import { Alert, AsyncStorage } from 'react-native';
+
+import {
+  cacheFonts,
+  cacheImages,
+  IFontSet,
+  IImageSet,
+} from '../../utils/cache';
+
+interface ILoadingScreenProps {
+  images?: IImageSet;
+  fonts?: IFontSet;
+  startAsync?: () => Promise<void>;
+  onFinish?: () => void;
+  onError?: (message?: any, ...params: any[]) => void;
+}
+
+export default class LoadingScreen extends React.Component<
+  ILoadingScreenProps
+> {
+  public render() {
+    return (
+      <AppLoading
+        startAsync={this.startAsync}
+        onFinish={this.onFinish}
+        onError={this.onError}
+      />
+    );
+  }
+
+  private startAsync = async () => {
+    const promises: Array<Promise<any>> = [];
+
+    if (this.props.images) {
+      const imageAssets = cacheImages(this.props.images);
+
+      promises.push(Promise.all([...imageAssets]));
+    }
+
+    if (this.props.fonts) {
+      const fontAssets = cacheFonts(this.props.fonts);
+
+      promises.push(Promise.all([...fontAssets]));
+    }
+
+    if (this.props.startAsync) {
+      promises.push(this.props.startAsync());
+    }
+
+    await Promise.all(promises);
+  };
+
+  private onFinish = () => {
+    if (this.props.onFinish) {
+      this.props.onFinish();
+    }
+  };
+
+  private onError = (message?: any, ...params: any[]) => {
+    if (this.props.onError) {
+      this.props.onError(message, params);
+    }
+  };
+}
